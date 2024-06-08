94ef66b8772bf0215e514240acbdd20fa2863dc7
Nikolay Iskhakov <nn.iskhakov@gmail.com>
2024-06-08 22:09:21 +0700

    Add Animals

---
+++ asp.net_01_Animals.cs/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Animals
+{
+    interface IFormatOut
+    {
+        void Show(Animal a);
+    }
+    class FormatFull : IFormatOut
+    {
+        public void Show(Animal a)
+        {
+            Console.WriteLine(a.Name + " " + a.Age.ToString() + " " + a.Sound + " " + a.Color);
+        }
+    }
+    class FormatShort : IFormatOut
+    {
+        public void Show(Animal a)
+        {
+            Console.WriteLine(a.Name);
+        }
+    }
+    class Animal
+    {
+        public string Name { get; private set; }
+        public string Sound { get; private set; }
+        public string Color { get; private set; }
+        public int Age { get; private set; }
+        public IFormatOut format { private get; set; }
+        public Animal(string name, string sound, string color, int age)
+        {
+            Name = name;
+            Sound = sound;
+            Color = color;
+            Age = age;
+        }
+        public void Show()
+        {
+            format.Show(this);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal a1 = new Animal("Cat", "mew", "black", 5);
+            a1.format = new FormatFull();
+            a1.Show();
+
+            Animal a2 = new Animal("Dog", "gav", "red", 7);
+            a2.format = new FormatFull();
+            a2.Show();
+
+            Animal a3 = new Animal("Bird", "chirik", "blue", 1);
+            a3.format = new FormatFull();
+            a3.Show();
+
+            a1.format = new FormatShort();
+            a2.format = new FormatShort();
+            a3.format = new FormatShort();
+
+            a1.Show();
+            a2.Show();
+            a3.Show();
+        }
+    }
+}
---
+++ asp.net_01_Animals.cs/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("asp.net_01_Animals.cs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("asp.net_01_Animals.cs")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f047328c-48d2-4c15-9112-78b0e3df7857")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
