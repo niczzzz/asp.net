e4debbf0ef12fc74a9dd035f2ee78753dabff0b9
niczzzz <nn.iskhakov@gmail.com>
2024-06-14 15:02:04 +0700

    Corr

--- 2024.06.09-main/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ 2024.06.09-main/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f67fa1356e5d3202f6392693fd94b919d4c6d9ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
